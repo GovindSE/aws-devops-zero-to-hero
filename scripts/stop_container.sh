5ce770d6cb734acf8bf2a99e89040189f24ce45e
GovindSE <107019172+GovindSE@users.noreply.github.com>
2025-04-15 23:37:03 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-containerid= sudo docker ps | awk 'NR>1 {print $1}'
+containerid=$(sudo docker ps | awk 'NR>1 {print $1}')
 
-if[ $containerid] then:
-echo "docker present"
-
-sudo docker rm -f $containerid
- echo "removed"
-
- else:
-
- echo "no running container"
+if [ "$containerid" ]; then
+  sudo docker rm -f "$containerid"
+  echo "Removed container $containerid"
+else
+  echo "No running containers to remove."
+fi
