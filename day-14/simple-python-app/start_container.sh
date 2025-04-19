42960c100e838f36804c2d494d6ceb062c771f73
GovindSE <107019172+GovindSE@users.noreply.github.com>
2025-04-19 23:07:17 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull govinds1209/demoapp
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 govinds1209/demoapp
